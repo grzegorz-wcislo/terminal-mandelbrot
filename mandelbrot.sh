fcb1a35ebda2a401ea39ff9b38b43a9904b250e6
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2018-04-11 17:44:44 +0200

    clean up code duplication

--- mandelbrot.sh
+++ mandelbrot.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
+DEFAULT_SCALE=33
+
 x=0
 y=0
-scale=33
-
-jx=0
-jy=0
-jscale=33
+scale=$DEFAULT_SCALE
 
 fractal=m
 
@@ -14,29 +12,16 @@ main() {
     init
     while :; do
         read -rsn1 -d ''
-        if [ $fractal = m ]; then
-            case "${REPLY}" in
-                w) y=$(echo "$y $scale" | awk '{printf "%f", $1+3/$2}') ;;
-                s) y=$(echo "$y $scale" | awk '{printf "%f", $1-3/$2}') ;;
-                d) x=$(echo "$x $scale" | awk '{printf "%f", $1+6/$2}') ;;
-                a) x=$(echo "$x $scale" | awk '{printf "%f", $1-6/$2}') ;;
-                q) scale=$(echo "$scale" | awk '{printf "%f", $1/1.1}') ;;
-                e) scale=$(echo "$scale" | awk '{printf "%f", $1*1.1}') ;;
-                j) switch_fractal ;;
-                ?) cleanup ;;
-            esac
-        else
-            case "${REPLY}" in
-                w) jy=$(echo "$jy $jscale" | awk '{printf "%f", $1+3/$2}') ;;
-                s) jy=$(echo "$jy $jscale" | awk '{printf "%f", $1-3/$2}') ;;
-                d) jx=$(echo "$jx $jscale" | awk '{printf "%f", $1+6/$2}') ;;
-                a) jx=$(echo "$jx $jscale" | awk '{printf "%f", $1-6/$2}') ;;
-                q) jscale=$(echo "$jscale" | awk '{printf "%f", $1/1.1}') ;;
-                e) jscale=$(echo "$jscale" | awk '{printf "%f", $1*1.1}') ;;
-                j) switch_fractal ;;
-                ?) cleanup ;;
-            esac
-        fi
+        case "${REPLY}" in
+            w) y=$(echo "$y $scale" | awk '{printf "%f", $1+3/$2}') ;;
+            s) y=$(echo "$y $scale" | awk '{printf "%f", $1-3/$2}') ;;
+            d) x=$(echo "$x $scale" | awk '{printf "%f", $1+6/$2}') ;;
+            a) x=$(echo "$x $scale" | awk '{printf "%f", $1-6/$2}') ;;
+            q) scale=$(echo "$scale" | awk '{printf "%f", $1/1.1}') ;;
+            e) scale=$(echo "$scale" | awk '{printf "%f", $1*1.1}') ;;
+            j) switch_fractal ;;
+            ?) cleanup ;;
+        esac
         draw
     done
 }
@@ -54,28 +39,29 @@ init() {
 
 draw() {
     tput cup 0 0
-    if [ $fractal = m ]; then
-        ./mandelbrot m $w $h $scale $x $y 100
-        tput cup 0 0
-        echo "Position: ($x,$y)"
-        echo "Zoom: $scale"
-    else
-        ./mandelbrot j $w $h $jscale $jx $jy 100 $x $y
-        tput cup 0 0
-        echo "Position: ($jx,$jy)"
-        echo "Zoom: $jscale"
-        echo "Parameter: ($x,i$y)"
+    ./mandelbrot $fractal $w $h $scale $x $y 100 $px $py
+    tput cup 0 0
+    echo "Position: ($x,$y)"
+    echo "Zoom: $scale"
+    if [ $fractal = j ]; then
+        echo "Parameter: ($px,$py)"
     fi
 }
 
 switch_fractal() {
     if [ $fractal = m ]; then
         fractal=j
-        jx=0
-        jy=0
-        jscale=33
+        px=$x
+        py=$y
+        m_scale=$scale;
+        x=0
+        y=0
+        scale=$DEFAULT_SCALE
     else
         fractal=m
+        x=$px
+        y=$py
+        scale=$m_scale
     fi
 }
 
