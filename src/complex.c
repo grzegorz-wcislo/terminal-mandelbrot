ac6e79544de1e7d466a8da392a89cf351665a3ce
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2018-04-13 21:48:27 +0200

    make returns more compact

--- src/complex.c
+++ src/complex.c
@@ -5,15 +5,13 @@
 /* Returns the square of a complex number */
 Complex square(Complex c)
 {
-    Complex res = {c.re*c.re - c.im*c.im, 2*c.re*c.im};
-    return res;
+    return (Complex) {c.re*c.re - c.im*c.im, 2*c.re*c.im};
 }
 
 /* Returns the sum of two complex numbers */
 Complex add(Complex c1, Complex c2)
 {
-    Complex res = {c1.re + c2.re, c1.im + c2.im};
-    return res;
+    return (Complex) {c1.re + c2.re, c1.im + c2.im};
 }
 
 /* Returns the absolute value of a complex number squared */
